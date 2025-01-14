1d33d2e6dd40553df598165bc42a111278d2b4f4
A-Hughes03 <adamhughesme1@gmail.com>
2025-01-13 23:34:15 -0500

    Add Angular Material and update styles for improved UI

--- convertly/src/app/app.module.ts
+++ convertly/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { TopBarComponent } from './shared/top-bar/top-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { TopBarComponent } from './shared/top-bar/top-bar.component';
     TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

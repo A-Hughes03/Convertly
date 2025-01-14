4e0b9752509ff9061f888b305c9b3384114b23b4
A-Hughes03 <adamhughesme1@gmail.com>
2025-01-13 23:50:25 -0500

    Add dark mode toggle component and integrate into top bar - test

--- convertly/src/app/app.module.ts
+++ convertly/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { TopBarComponent } from './shared/top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DarkModeToggleComponent } from './shared/top-bar/dark-mode-toggle/dark-mode-toggle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TopBarComponent
+    TopBarComponent,
+    DarkModeToggleComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- convertly/src/app/shared/top-bar/top-bar.component.ts
+++ convertly/src/app/shared/top-bar/top-bar.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent {
-
+  handleDarkModeChange(isDarkMode: boolean): void {
+    console.log('Dark mode:', isDarkMode);
+  }
 }

0dcb11a564b387ed6a266ceda40bc05f7658bb45
A-Hughes03 <adamhughesme1@gmail.com>
2025-01-14 21:11:08 -0500

    test: update component tests to include TopBar, DarkModeToggle, and SocialMediaButton components

--- convertly/src/app/app.component.spec.ts
+++ convertly/src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { TopBarComponent } from './shared/top-bar/top-bar.component';
+import { DarkModeToggleComponent } from './shared/top-bar/dark-mode-toggle/dark-mode-toggle.component';
+import { SocialMediaButtonComponent } from './shared/social-media-button/social-media-button.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent, HeaderComponent]
+    declarations: [AppComponent, HeaderComponent, TopBarComponent, DarkModeToggleComponent, SocialMediaButtonComponent]
   }));
 
   it('should create the app', () => {
--- convertly/src/app/shared/top-bar/top-bar.component.spec.ts
+++ convertly/src/app/shared/top-bar/top-bar.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TopBarComponent } from './top-bar.component';
+import { DarkModeToggleComponent } from './dark-mode-toggle/dark-mode-toggle.component';
+import { SocialMediaButtonComponent } from '../social-media-button/social-media-button.component';
 
 describe('TopBarComponent', () => {
   let component: TopBarComponent;
@@ -8,7 +10,7 @@ describe('TopBarComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [TopBarComponent]
+      declarations: [TopBarComponent, SocialMediaButtonComponent, DarkModeToggleComponent]
     });
     fixture = TestBed.createComponent(TopBarComponent);
     component = fixture.componentInstance;

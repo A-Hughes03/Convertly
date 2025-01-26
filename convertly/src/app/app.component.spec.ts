9b9572b79dfd4945f30e0ad30f435e3cdaa74046
A-Hughes03 <adamhughesme1@gmail.com>
2025-01-26 12:38:43 -0500

    add FileConverterComponent and update tests with necessary Angular Material modules

--- convertly/src/app/app.component.spec.ts
+++ convertly/src/app/app.component.spec.ts
@@ -4,10 +4,30 @@ import { HeaderComponent } from './shared/header/header.component';
 import { TopBarComponent } from './shared/top-bar/top-bar.component';
 import { DarkModeToggleComponent } from './shared/top-bar/dark-mode-toggle/dark-mode-toggle.component';
 import { SocialMediaButtonComponent } from './shared/social-media-button/social-media-button.component';
+import { FileConverterComponent } from './shared/file-converter/file-converter.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    declarations: [AppComponent, HeaderComponent, TopBarComponent, DarkModeToggleComponent, SocialMediaButtonComponent]
+    declarations: [
+      AppComponent,
+      HeaderComponent,
+      TopBarComponent,
+      DarkModeToggleComponent,
+      SocialMediaButtonComponent,
+      FileConverterComponent
+    ],
+    imports: [
+      MatFormFieldModule,
+      MatSelectModule,
+      MatIconModule,
+      FormsModule,
+      BrowserAnimationsModule
+    ]
   }));
 
   it('should create the app', () => {
--- convertly/src/app/shared/file-converter/file-converter.component.spec.ts
+++ convertly/src/app/shared/file-converter/file-converter.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FileConverterComponent } from './file-converter.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FileConverterComponent', () => {
   let component: FileConverterComponent;
@@ -8,7 +12,14 @@ describe('FileConverterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FileConverterComponent]
+      declarations: [FileConverterComponent],
+      imports: [
+        MatFormFieldModule,
+        MatSelectModule,
+        MatIconModule,
+        FormsModule,
+        BrowserAnimationsModule
+      ]
     });
     fixture = TestBed.createComponent(FileConverterComponent);
     component = fixture.componentInstance;
